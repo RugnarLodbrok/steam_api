787d92226b85b4afa0e4586d3d1edef4eb7fda4b
Georgy Usatenko <georgy.usatenko@picsart.com>
2024-03-14 00:23:04 +0300

    test cache classmethod

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -35,6 +35,20 @@ def func_no_args():
     return foo
 
 
+@pytest.fixture()
+def cached_method(cacher):
+    class A:
+        def __init__(self):
+            self.counter = 0
+
+        @cacher('prefix', key='no_self', model=None)
+        def foo(self, arg1, arg2):
+            self.counter += 1
+            return TestDatum(name=str(self.counter), arg=f'{arg1}@{arg2}').model_dump()
+
+    return A().foo
+
+
 @pytest.fixture
 def cache_path() -> Path:
     return Path(__file__).parent / 'test_cache'
@@ -47,16 +61,25 @@ def cacher(cache_path):
 
 
 def test_cache(cacher, func_one_arg, cache_path):
-    cached_foo = cacher('subpath', key='all_str', model=TestDatum)(func_one_arg)
+    cached_foo = cacher('prefix', key='all_str', model=TestDatum)(func_one_arg)
     cached_foo('ARG')
     result = cached_foo('ARG')
     assert result == TestDatum(name='a', arg='ARG')
-    assert (cache_path / 'subpath' / 'arg.yml').read_text() == 'arg: ARG\nname: a\n'
+    assert (cache_path / 'prefix' / 'arg.yml').read_text() == 'arg: ARG\nname: a\n'
 
 
 def test_cache_singleton(func_no_args, cacher, cache_path):
-    cached_foo = cacher('subpath', key=None, model=TestDatum)(func_no_args)
+    cached_foo = cacher('prefix', key=None, model=TestDatum)(func_no_args)
     cached_foo()
     result = cached_foo()
     assert result == TestDatum(name='a')
-    assert (cache_path / 'subpath.yml').read_text() == 'name: a\n'
+    assert (cache_path / 'prefix.yml').read_text() == 'name: a\n'
+
+
+def test_cache_method(cached_method, cache_path):
+    cached_method('a', 'b')
+    cached_method('a', 'c')
+    result = cached_method('a', 'c')
+    assert result == {'arg': 'a@c', 'name': '2'}
+    assert (cache_path / 'prefix' / 'a_b.yml').read_text() == "arg: a@b\nname: '1'\n"
+    assert (cache_path / 'prefix' / 'a_c.yml').read_text() == "arg: a@c\nname: '2'\n"
